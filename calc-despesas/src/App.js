755bce5c4e501404bf70cb34ce0f4d762bfb12e6
ErickCassoli <erick.mcassoli@gmail.com>
2023-11-27 21:50:01 -0300

    feat: update a app.js

--- calc-despesas/src/App.js
+++ calc-despesas/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+// src/App.js
+import React, { useState } from 'react';
+import Cadastro from './components/cadastro';
+import Despesas from './components/Despesas';
+import Receitas from './components/Receitas';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [despesas, setDespesas] = useState([]);
+  const [receitas, setReceitas] = useState([]);
+
+  const handleAdd = (item) => {
+    if (item.tipo === 'despesa') {
+      setDespesas([...despesas, item]);
+    } else {
+      setReceitas([...receitas, item]);
+    }
+  };
+
+  const handleDelete = (id, tipo) => {
+    if (tipo === 'despesa') {
+      setDespesas(despesas.filter((despesa) => despesa.id !== id));
+    } else {
+      setReceitas(receitas.filter((receita) => receita.id !== id));
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="content">
+      <Cadastro onAdd={handleAdd} />
+      <Despesas despesas={despesas} onDelete={handleDelete} />
+      <Receitas receitas={receitas} onDelete={handleDelete} />
     </div>
   );
-}
+};
 
 export default App;
